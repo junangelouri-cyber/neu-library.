9d2e89e307ac9fb3008a2419fbb41424fc562bbd
junangelouri-cyber <junangelo.uri@neu.edu.ph>
2026-03-20 17:46:56 +0800

    Delete download/src directory

--- download/src/context/AuthContext.tsx
+++
@@ -1,3 +0,0 @@
-// This file is deprecated. Please use the hooks from '@/firebase' to get user and profile information.
-// The functionality has been migrated to the FirebaseProvider and the login page.
-export {};
--- download/src/lib/firebase.ts
+++
@@ -1,3 +0,0 @@
-// This file is deprecated. Please use the exports from '@/firebase' instead.
-// For example: import { useAuth, useFirestore } from '@/firebase';
-export {};
